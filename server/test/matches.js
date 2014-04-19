26e20eaaa2d15e5d6335a1a8345c5bca8545d839
ajlopez <webmaster@ajlopez.com>
2014-04-19 13:11:30 -0300

    Refactor test to use simpleflow

--- server/test/matches.js
+++ server/test/matches.js
@@ -1,5 +1,10 @@
 
 var matches = require('../services/matches');
+var simpleflow = require('simpleflow');
+
+function errorfn(err) {
+    console.log(err);
+}
 
 exports['get empty list'] = function (test) {
     test.async();
@@ -29,48 +34,64 @@ exports['add and get match by id'] = function (test) {
     
     var match = { local: 'argentina', away: 'brazil' };
     
-    matches.add(match, function (err, id) {
-        test.ok(!err);
-        test.ok(id);
-        
-        matches.getById(id, function (err, item) {
-            test.ok(!err);
-            test.ok(item);
-            test.equal(item.id, id);
-            test.equal(item.local, 'argentina');
-            test.equal(item.away, 'brazil');
-            test.done();
-        });
-    });
+    var flow = simpleflow.createFlow([add, getById, done], errorfn);
+    
+    flow.run(match);
+    
+    function add(data, next) {
+        matches.add(data, next);
+    }
+    
+    var id;
+    
+    function getById(data, next) {
+        id = data;
+        matches.getById(id, next);
+    }
+    
+    function done(item) {
+        test.ok(item);
+        test.equal(item.id, id);
+        test.equal(item.local, 'argentina');
+        test.equal(item.away, 'brazil');
+        test.done();
+    };
 };
 
 exports['load list and get list'] = function (test) {
     test.async();
+    
+    var flow = simpleflow.createFlow([clear, addList, getList, done], errorfn);
     
     var list = [
         { local: 'argentina', away: 'brazil' },
         { local: 'bosnia', away: 'germany' }
     ];
     
-    matches.clear(function (err, data) {
-        test.ok(!err);
+    function clear(data, next) {
+        matches.clear(next);
+    }
+    
+    function addList(data, next) {
+        matches.addList(list, next);
+    }
+    
+    function getList(data, next) {
+        matches.getList(next);
+    }
+    
+    function done(data) {
+        test.ok(data);
+        test.ok(Array.isArray(data));
+        test.equal(data.length, 2);
         
-        matches.addList(list, function (err, data) {
-            test.ok(!err);
-            
-            matches.getList(function (err, data) {
-                test.ok(!err);
-                test.ok(data);
-                test.ok(Array.isArray(data));
-                test.equal(data.length, 2);
-                
-                test.equal(data[0].local, 'argentina');
-                test.equal(data[0].away, 'brazil');
-                test.equal(data[1].local, 'bosnia');
-                test.equal(data[1].away, 'germany');
-                test.done();
-            });
-        });
-    });
+        test.equal(data[0].local, 'argentina');
+        test.equal(data[0].away, 'brazil');
+        test.equal(data[1].local, 'bosnia');
+        test.equal(data[1].away, 'germany');
+        test.done();
+    }
+    
+    flow.run();
 };
 
