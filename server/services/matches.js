4bf1ac36e22756f908ad3696dfd0cbc82f403cb5
ajlopez <webmaster@ajlopez.com>
2014-04-21 19:20:55 -0300

    Matches memory provider

--- server/services/matches.js
+++ server/services/matches.js
@@ -1,72 +1,129 @@
 
-var maxid = 0;
-var matches = [];
-
-function add(match, cb) {
-    maxid++;
-    var item = clone(match);
-    item.id = maxid;
-    matches[item.id] = item;
-    cb(null, item.id);
-};
-
-function getById(id, cb) {
-    cb(null, matches[id]);
-};
-
-function getList(options, cb) {
-    if (typeof options == 'function' && !cb) {
-        cb = options;
-        options = null;
-    }
+function MatchesDb(db) {
+    var repository = mongodb.createRepository(db, 'matches');
     
-    options = options || { };
-    
-    var result = [];
-    
-    matches.forEach(function (match) {
-        if (options.date && match.date != options.date)
-            return;
+    this.clear = function (cb) {
+        repository.clear(cb);
+    };
+
+    this.add = function (match, cb) {
+        repository.insert(match, function (err, records) {
+            if (err) {
+                cb(err, null);
+                return;
+            }
             
-        result.push(clone(match));
-    });
+            cb(null, records[0]._id.toString());
+        });
+    };
     
-    cb(null, result);
+    this.getList = function (cb) {
+        repository.findAll(function (err, items) {
+            if (err) {
+                cb(err, null);
+                return;
+            }
+            
+            items.forEach(function(item) {
+                item.id = item._id.toString();
+            });
+            
+            cb(null, items);
+        });
+    };
+    
+    this.getById = function (id, cb) {
+        repository.findById(id, function (err, item) {
+            if (err) {
+                cb(err, null);
+                return;
+            }
+            
+            item.id = item._id.toString();
+            
+            cb(null, item);
+        });
+    };
 };
 
-function addList(list, options, cb) {
-    if (options && !cb && typeof options == 'function') {
-        cb = options;
-        options = null;
+function MatchesMemory() {
+    var maxid = 0;
+    var matches = [];
+
+    this.add = function (match, cb) {
+        maxid++;
+        var item = clone(match);
+        item.id = maxid;
+        matches[item.id] = item;
+        cb(null, item.id);
+    };
+
+    this.getById = function (id, cb) {
+        cb(null, matches[id]);
+    };
+
+    this.getList = function (options, cb) {
+        if (typeof options == 'function' && !cb) {
+            cb = options;
+            options = null;
+        }
+        
+        options = options || { };
+        
+        var result = [];
+        
+        matches.forEach(function (match) {
+            if (options.date && match.date != options.date)
+                return;
+                
+            result.push(clone(match));
+        });
+        
+        cb(null, result);
+    };
+
+    this.addList = function (list, options, cb) {
+        var self = this;
+        
+        if (options && !cb && typeof options == 'function') {
+            cb = options;
+            options = null;
+        }
+        
+        options = options || { };
+        
+        var k = 0;
+        var l = list.length;
+        
+        doStep();
+        
+        function doStep() {
+            if (k >= l) {
+                cb(null, null);
+                return;
+            }
+            
+            var item = clone(list[k++]);
+            
+            if (item.date && options.date && item.date > options.date) {
+                delete item.localgoals;
+                delete item.awaygoals;
+            }
+            else if (item.date && options.date && item.date == options.date && (!options.time || item.time >= options.time)) {
+                delete item.localgoals;
+                delete item.awaygoals;
+            }
+            
+            self.add(item, function () { setImmediate(function () { doStep(); }) });
+        }
+    };
+
+    this.clear = function (cb) {
+        maxid = 0;
+        matches = [];
+        cb(null, null);
     }
-    
-    options = options || { };
-    
-    var k = 0;
-    var l = list.length;
-    
-    doStep();
-    
-    function doStep() {
-        if (k >= l) {
-            cb(null, null);
-            return;
-        }
-        
-        var item = clone(list[k++]);
-        
-        if (item.date && options.date && item.date > options.date) {
-            delete item.localgoals;
-            delete item.awaygoals;
-        }
-        else if (item.date && options.date && item.date == options.date && (!options.time || item.time >= options.time)) {
-            delete item.localgoals;
-            delete item.awaygoals;
-        }
-        
-        add(item, function () { setImmediate(function () { doStep(); }) });
-    }
-};
+}
 
 function clone(obj) {
     var newobj = { };
@@ -77,10 +134,26 @@ function clone(obj) {
     return newobj;
 }
 
+var provider = new MatchesMemory();
+
 function clear(cb) {
-    maxid = 0;
-    matches = [];
-    cb(null, null);
+    provider.clear(cb);
+}
+
+function add(match, cb) {
+    provider.add(match, cb);
+}
+
+function getList(options, cb) {
+    provider.getList(options, cb);
+}
+
+function addList(list, options, cb) {
+    provider.addList(list, options, cb);
+}
+
+function getById(id, cb) {
+    provider.getById(id, cb);
 }
 
 module.exports = {
